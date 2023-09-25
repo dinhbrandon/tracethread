ba914a3d9677204d5545d594f0677c00272596e9
karbassioon-d <karbasion.dara@gmail.com>
2023-09-25 13:09:29 -0700

    started making the Login form

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,4 +1,10 @@
-const LoginForm = () => {
+import { useState, ChangeEvent, FormEvent } from 'react';
+
+interface LoginFormProps {
+  onLogin: (email: string, password: string) => void;
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ onLogin }) => {
   return (
     <div>
       Login
