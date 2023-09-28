a955ce8ad71f095d570d8ee089b8faffbb4f0dce
karbassioon-d <karbasion.dara@gmail.com>
2023-09-28 13:27:09 -0700

    added some comments to the store.tsx for clarity

--- frontend/src/components/redux/store.tsx
+++ frontend/src/components/redux/store.tsx
@@ -5,24 +5,30 @@ import storage from 'redux-persist/lib/storage';
 import authReducer from './authSlice';
 import jobReducer from './jobSlice';
 
+//Configure object for redux-persist for the auth reducer
 const persistConfigAuth = {
   key: 'auth',
   storage,
 };
 
+// Configuration object for redux-persist for the job reducer
 const persistConfigJob = {
   key: 'job',
   storage,
 };
 
+// Creating persisted reducers using the configuration objects
 const persistedAuthReducer = persistReducer(persistConfigAuth, authReducer);
 const persistedJobReducer = persistReducer(persistConfigJob, jobReducer);
 
+// Customized middleware to ignore serializableCheck
+// This is usually done to avoid warnings when non-serializable values are used
 const customizedMiddleware = (getDefaultMiddleware: any) =>
   getDefaultMiddleware({
     serializableCheck: false,
   });
 
+// Configuring the store with reducers, middleware, and enhancers
 const store = configureStore({
   reducer: {
     auth: persistedAuthReducer,
@@ -31,6 +37,7 @@ const store = configureStore({
   middleware: customizedMiddleware
 });
 
+// Defining a type for thunk actions to be used in async action creators
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
@@ -38,9 +45,11 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   Action<string>
 >;
 
+// Defining the RootState type based on the state shape of the store
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch & ThunkDispatch<RootState, unknown, Action<string>>;
 
+// Creating a persistor to enable persisted state
 export const persistor = persistStore(store);
 
 export default store;
