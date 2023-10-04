9208291e3645cebe0f918a962507256b5d7408ce
Brandon Dinh <dinhbrandon@gmail.com>
2023-10-03 17:36:30 -0700

    date in joblisting model now str to handle serializer date logic

--- backend/scripts/data-pipeline/data_serializer.py
+++ backend/scripts/data-pipeline/data_serializer.py
@@ -12,4 +12,66 @@ Responsibilities:
 This component acts as an intermediary step between stream processing and data storage or transmission.
 """
 
-pass
+import json
+import os
+from datetime import datetime
+
+# Function to clean the job description by removing site-specific text and replacing newlines with <br>
+
+
+def clean_description(description):
+    # Remove 'Show more' and 'Show less'
+    description = description.replace('Show more', '').replace('Show less', '')
+    
+    # Replace newlines with <br> for HTML rendering
+    description = description.replace('\n', '<br>')
+    
+    return description.strip()
+
+
+def clean_and_transform_data(raw_data_dict):
+    """
+    Clean and transform the raw scraped data.
+    This function can be expanded based on specific cleaning and transformation needs.
+    """
+
+    for key in raw_data_dict:
+        # Convert datetime object to 'MM/DD/YYYY 00:00AM/PM' format
+        if isinstance(raw_data_dict['date'], datetime):
+            raw_data_dict['date'] = raw_data_dict['date'].strftime('%m/%d/%Y %I:%M%p')
+
+        if raw_data_dict['description'] is not None:
+            raw_data_dict['description'] = clean_description(raw_data_dict['description'])
+
+        # Ensure that all keys have values (even if None) to maintain a consistent JSON structure
+        for key in ['job_title', 'company_name', 'listing_details', 'description', 'location', 'date', 'url']:
+            if key not in raw_data_dict:
+                raw_data_dict[key] = None
+    return raw_data_dict
+
+
+def save_processed_data(processed_data):
+    """
+    Save the processed data back to a JSON file.
+    """
+    # Determine the directory of the current script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    
+    # Generate the filename using the current datetime
+    current_datetime = datetime.now().strftime('%Y%m%d_%H%M%S')
+    filename = f"job_data_{current_datetime}.json"
+    
+    # Set the filepath to the 'serialized-job-data' directory using an absolute path
+    filepath = os.path.join(script_dir, 'serialized-job-data', filename)
+
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+    try:
+        print(f"Saving data to {filepath}...")
+        with open(filepath, "w") as file:
+            json.dump(processed_data, file, indent=4)
+        print(f"Data saved successfully to {filepath}!")
+    except Exception as e:
+        print(f"Error encountered while saving data: {e}")
+
