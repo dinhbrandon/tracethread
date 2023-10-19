7352a1ef2a95ec02260ad35dcccc7e9d7bd12bc5
Brandon Dinh <dinhbrandon@gmail.com>
2023-10-19 12:02:39 -0700

    implement pagination UI and search button change

--- frontend/src/components/JobSearch.tsx
+++ frontend/src/components/JobSearch.tsx
@@ -21,7 +21,7 @@ const JobSearch = () => {
   const [showAdvancedSearch, setShowAdvancedSearch] = useState(false);
   const [searchText, setSearchText] = useState('');
   const [isSimpleSearchLocked, setIsSimpleSearchLocked] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  // const [errorMessage, setErrorMessage] = useState('');
   const [hasSearched, setHasSearched] = useState(false);
 
   const handleSearchTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -30,9 +30,11 @@ const JobSearch = () => {
 
 const handleSearchButtonClick = () => {
   if (!searchText.trim()) {
-    setErrorMessage('Please enter a search query.');
+    // setErrorMessage('Please enter a search query.');
+    setSearchUrl(`http://localhost:8000/querier/search-job-listing/?q=`);
+    setHasSearched(true);
   } else {
-    setErrorMessage('');
+    // setErrorMessage('');
     setSearchUrl(`http://localhost:8000/querier/search-job-listing/?q=%28job_title%3D'${searchText}'%20%7C%20company_name%3D'${searchText}'%29`);
     setHasSearched(true);
   }
@@ -94,70 +96,73 @@ const handleSearchButtonClick = () => {
                 <div className="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden">
                     <div className="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 ">
                       <div className="sm:col-span-1">
-                        <div className="relative">
-                          <input
-                            type="text"
-                            value={searchText}
-                            onChange={handleSearchTextChange}
-                            className="py-2 px-3 pl-11 block w-full border-gray-200 shadow-sm rounded-md text-sm"
-                            placeholder="Search"
-                            disabled={isSimpleSearchLocked}
-                            />
-                          <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none pl-4">
-                            <svg className="h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                            </svg>
-                          </div>
-                        </div>
-                        {errorMessage && <p className="text-red-500 text-sm mt-2">{errorMessage}</p>}
-                      </div>
-                      <button disabled={isSimpleSearchLocked} onClick={handleSearchButtonClick} className="py-1 px-3 rounded-md border font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 transition-all text-sm">
-                          Search
-                      </button>
 
-                      <div className="sm:col-span-2 md:grow">
-                        <div className="flex justify-end gap-x-2">
-                          <div className="hs-dropdown relative inline-block [--placement:bottom-right]" data-hs-dropdown-auto-close="inside">
-                            <button onClick={toggleAdvancedSearch} id="hs-as-table-table-filter-dropdown" type="button" className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm">
-                              <svg className="w-3 h-3" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
-                              </svg>
-                              Advanced search
-                            </button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    {hasSearched && (
-                      <div className='mt-5 mb-5'>
-                        <h2 className="text-xl font-semibold text-gray-700">
-                            Search Results
-                        </h2>
-                        <p className="text-sm text-gray-600">
-                            View job details, save jobs, and more.
-                        </p>
-                      </div>
-                    )}
-                </div>
+                      <form
+  onSubmit={(e) => {
+    e.preventDefault();
+    handleSearchButtonClick();
+  }}
+>
+  <div className="sm:col-span-1">
+    <div className="relative">
+      <input
+        type="text"
+        value={searchText}
+        onChange={handleSearchTextChange}
+        className="py-2 px-3 pl-11 block w-full border-gray-200 shadow-sm rounded-md text-sm"
+        placeholder="Search"
+        disabled={isSimpleSearchLocked}
+      />
+      <div className="absolute inset-y-0 left-0 flex items-center pl-4">
+        <button type="submit" disabled={isSimpleSearchLocked} className="p-1">
+          <svg className="h-4 w-4 text-gray-400 hover:h-5 hover:w-5 transition duration-700" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078,.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+          </svg>
+        </button>
+      </div>
+    </div>
+    {/* {errorMessage && <p className="text-red-500 text-sm mt-2">{errorMessage}</p>} */}
+  </div>
+</form>
 
-                <div>
-                    {searchUrl && <SearchResults encodedQuery={searchUrl} />}
-                </div>
 
-            </div>
+
+  </div>
+
+
+  <div className="sm:col-span-2 md:grow">
+    <div className="flex justify-end gap-x-2">
+      <div className="hs-dropdown relative inline-block [--placement:bottom-right]" data-hs-dropdown-auto-close="inside">
+        <button onClick={toggleAdvancedSearch} id="hs-as-table-table-filter-dropdown" type="button" className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm">
+          <svg className="w-3 h-3" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+          </svg>
+          Advanced search
+        </button>
         </div>
+      </div>
+    </div>
+  </div>
+</div>
+{hasSearched && (
+  <div className='mt-5 mb-5'>
+    <h2 className="text-xl font-semibold text-gray-700">
+        Search Results
+    </h2>
+    <p className="text-sm text-gray-600">
+        View job details, save jobs, and more.
+    </p>
+  </div>
+)}
+      </div>
 
+      <div>
+          {searchUrl && <SearchResults encodedQuery={searchUrl} />}
+      </div>
 
+  </div>
+  </div>
 
-
-
-      {/* {showModal && 
-        <div className="bg-green-500 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 rounded-lg z-10">
-            {modalMessage}
-            <button onClick={handleCloseModal} className="mt-2 px-4 py-2 bg-white rounded">Close</button>
-        </div>
-      } */}
     </div>
 
 
--- frontend/src/components/SearchResults.tsx
+++ frontend/src/components/SearchResults.tsx
@@ -200,33 +200,57 @@ const SearchResults = ({ encodedQuery }: SearchResultsProps) => {
             }
 
 {hasResults && results.length > itemsPerPage ? (
-                <div className="flex justify-center mt-4">
-                    <button onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
-                    className="text-blue-500 mx-2"
-                    disabled={currentPage === 1}
+    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+
+        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+            <div>
+                <p className="text-sm text-gray-700">
+                    Showing
+                    <span className="font-medium"> {Math.min(currentPage * itemsPerPage, results.length)} </span>
+                    of
+                    <span className="font-medium"> {results.length} </span>
+                    results
+                </p>
+            </div>
+            <div>
+                <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                        <button
+                        onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+                        className={`relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 ${currentPage === 1 ? 'cursor-not-allowed' : ''}`}
+                        disabled={currentPage === 1}
                     >
-                        Prev
+                        <span className="sr-only">Previous</span>
+                        <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
+                        </svg>
                     </button>
                     {pageNumbers.map(number => (
-                        <div 
+                        <a 
+                            href="#" 
                             key={number} 
-                            className={`mx-2 cursor-pointer 
-                                ${currentPage === number ? 'text-white bg-blue-500' : 'text-blue-500'}`}
                             onClick={() => paginate(number)}
+                            className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${currentPage === number ? 'bg-indigo-600 text-white' : 'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50'}`}
                         >
-                            <a onClick={() => paginate(number)} href="#" className="text-black">
-                                {number}
-                            </a>
-                        </div>
+                            {number}
+                        </a>
                     ))}
-                    <button onClick={() => setCurrentPage(prev => Math.min(prev + 1, pageNumbers.length))}
-                    className="text-blue-500 mx-2"
-                    disabled={currentPage === pageNumbers.length}
+                    <button
+                        onClick={() => setCurrentPage(prev => Math.min(prev + 1, pageNumbers.length))}
+                        className={`relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 ${currentPage === pageNumbers.length ? 'cursor-not-allowed' : ''}`}
+                        disabled={currentPage === pageNumbers.length}
                     >
-                        Next
+                        <span className="sr-only">Next</span>
+                        <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+                        </svg>
                     </button>
-                </div>
-            ) : null}
+                </nav>
+            </div>
+        </div>
+    </div>
+) : null}
+
+
 
             <Modal isVisible={isModalVisible} status={status} onClose={() => setIsModalVisible(false)} />
 
