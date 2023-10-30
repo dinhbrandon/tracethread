95aac7d6d73e0ba2bece9ff34f4188e73533b8a2
Brandon Dinh <dinhbrandon@gmail.com>
2023-10-30 15:20:33 -0700

    configure DB build

--- backend/manage.py
+++ backend/manage.py
@@ -2,19 +2,26 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
     # Check the DJANGO_ENV environment variable to determine which settings file to use
-    DJANGO_ENV = os.environ.get('DJANGO_ENV', 'local')  # default to local if not set
+    DJANGO_ENV = os.environ.get('DJANGO_ENV', 'production')  # default to local if not set
 
     if DJANGO_ENV == 'production':
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracethread_proj.settings.production")
+        dotenv_path = Path(__file__).resolve().parent.parent.parent / '.env.production'
+        load_dotenv(dotenv_path=dotenv_path)
         print("This is the production environment")
     else:
+        dotenv_path = Path(__file__).resolve().parent.parent.parent.parent / '.env'
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracethread_proj.settings.local")
         print("This is the local environment")
+
+    load_dotenv(dotenv_path=dotenv_path)
 
     try:
         from django.core.management import execute_from_command_line
