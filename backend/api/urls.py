e9849af8308f1cb9cc3f374a07151c81b442c7bd
karbassioon-d <karbasion.dara@gmail.com>
2023-10-31 12:42:27 -0700

    added health_check page

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import FeedbackCreateView
+from .views import FeedbackCreateView, HealthCheckView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('submit-feedback/', FeedbackCreateView.as_view()),
+    path('health_check/', HealthCheckView.as_view(), name='health_check'),
+    path('health_check.html', TemplateView.as_view(template_name="health_check.html"), name='health_check_html'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .serializers import FeedbackSerializer
 from .models import Feedback
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from django.db import connections
+from django.core.cache import cache
+from rest_framework.response import Response
 # Create class based view to create an instance of feedback
 
 
@@ -10,3 +14,26 @@ class FeedbackCreateView(generics.CreateAPIView):
     serializer_class = FeedbackSerializer
     queryset = Feedback.objects.all()
     permission_classes = [AllowAny, ]
+
+
+class HealthCheckView(APIView):
+    
+    def get(self, request, format=None):
+        # Check database connectivity
+        db_ok = True
+        for conn in connections.all():
+            try:
+                conn.cursor()
+            except Exception:
+                db_ok = False
+                break
+
+        # Check cache connectivity
+        cache_ok = cache.set('health_check_key', 'value', timeout=30)
+
+        if db_ok and cache_ok:
+            data = {"status": "ok"}
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            data = {"status": "error"}
+            return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
