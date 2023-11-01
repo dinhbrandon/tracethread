d546eaafae2f036d9bdae89046d07e9ff4dcd9c1
Brandon Dinh <dinhbrandon@gmail.com>
2023-11-01 14:03:58 -0700

    change health check view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render
 from .serializers import FeedbackSerializer
 from .models import Feedback
 from rest_framework import generics
-from django.http import HttpResponse
 from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
-from django.db import connections
-from django.core.cache import cache
-from rest_framework.response import Response
+
 # Create class based view to create an instance of feedback
 
 
@@ -16,12 +11,3 @@ class FeedbackCreateView(generics.CreateAPIView):
     queryset = Feedback.objects.all()
     permission_classes = [AllowAny, ]
 
-
-class HealthCheckMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        if request.path == '/health':
-            return HttpResponse('ok')
-        return self.get_response(request)
