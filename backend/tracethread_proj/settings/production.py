33104d94a4d8abdef09975c3deb226f6b2bb20c5
karbassioon-d <karbasion.dara@gmail.com>
2023-11-02 14:03:53 -0700

    can now upload feedback screenshots to s3

--- backend/tracethread_proj/settings/production.py
+++ backend/tracethread_proj/settings/production.py
@@ -13,6 +13,16 @@ DB_PASSWORD = env("POSTGRES_PASSWORD")
 
 DEBUG = False
 
+
+#AWS S3 Bucket Config
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+# AWS_DEFAULT_ACL = 'public-read'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
 ALLOWED_HOSTS = [
     "www.tracethread.com",
     "api.tracethread.com",
