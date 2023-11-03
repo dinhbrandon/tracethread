c09a28338d08ab1a4c2fdbb5303c2682d782f7e4
Brandon Dinh <dinhbrandon@gmail.com>
2023-11-03 16:08:37 -0700

    fix add comment functionality to accept ID through HTTP request URL.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('submit-feedback/', FeedbackCreateView.as_view()),
     path('list-feedback/', FeedbackListView.as_view()),
     path('upvote-feedback/<int:pk>/', FeedbackUpvoteView.as_view()),
-    path('submit-comments/', CommentsCreateView.as_view()),
+    path('submit-comments/<int:feedback>/', CommentsCreateView.as_view()),
     path('list-comments/<int:pk>/', CommentsListView.as_view()),
-    path('upvote-comments/<int:pk>/', CommentsUpvoteView.as_view()),   
+    path('upvote-comments/<int:pk>/', CommentsUpvoteView.as_view()),
 ]
