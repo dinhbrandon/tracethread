f80845c4027f2a859d4b194339f5899d2a48d4db
Brandon Dinh <dinhbrandon@gmail.com>
2023-11-03 13:28:56 -0700

    add upvote functionality for feedback, add comments on feedback

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# Allow commenting on Feedback instances, and also upvotes on comments - comments would have a foreign key to the Feedback instance
+# Feedback should be able to have multiple comments, and comments should be able to have upvotes that can be incremented
+
+
+class Upvote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    feedback = models.ForeignKey('Feedback', on_delete=models.CASCADE, null=True, blank=True)
+    comment = models.ForeignKey('Comments', on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        unique_together = (('user', 'feedback'), ('user', 'comment'))
+
+
+class Comments(models.Model):
+    feedback = models.ForeignKey('Feedback', on_delete=models.CASCADE)
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def upvotes_count(self):
+        return Upvote.objects.filter(comment=self).count()
+
+    def __str__(self):
+        return self.comment
 
 
 class Feedback(models.Model):
@@ -6,6 +32,14 @@ class Feedback(models.Model):
     feedback = models.TextField()
     screenshot = models.ImageField(upload_to='feedback', null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def comments(self):
+        return Comments.objects.filter(feedback=self)
+    
+    @property
+    def upvotes_count(self):
+        return Upvote.objects.filter(feedback=self).count()
 
     def __str__(self):
         return self.url
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,65 @@
-from .serializers import FeedbackSerializer
-from .models import Feedback
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
-
-# Create class based view to create an instance of feedback
+from .serializers import FeedbackSerializer, CommentsSerializer
+from .models import Feedback, Comments, Upvote
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class FeedbackCreateView(generics.CreateAPIView):
     serializer_class = FeedbackSerializer
     queryset = Feedback.objects.all()
-    permission_classes = [AllowAny, ]
+    permission_classes = [IsAuthenticated, ]
 
+
+class FeedbackListView(generics.ListAPIView):
+    serializer_class = FeedbackSerializer
+    queryset = Feedback.objects.all()
+    permission_classes = [IsAuthenticated,]
+
+
+
+class FeedbackUpvoteView(generics.UpdateAPIView):
+    serializer_class = FeedbackSerializer
+    queryset = Feedback.objects.all()
+    permission_classes = [IsAuthenticated,]
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        user = request.user
+        upvote, created = Upvote.objects.get_or_create(user=user, feedback=instance)
+
+        if not created:
+            return Response({'detail': 'You have already upvoted this feedback.'}, status=status.HTTP_400_BAD_REQUEST)    
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+class CommentsCreateView(generics.CreateAPIView):
+    serializer_class = CommentsSerializer
+    queryset = Feedback.objects.all()
+    permission_classes = [IsAuthenticated, ]
+
+
+class CommentsListView(generics.ListAPIView):
+    serializer_class = CommentsSerializer
+    queryset = Feedback.objects.all()
+    permission_classes = [IsAuthenticated,]
+
+
+class CommentsUpvoteView(generics.UpdateAPIView):
+    serializer_class = CommentsSerializer
+    queryset = Comments.objects.all()  # Updated queryset to Comments
+    permission_classes = [IsAuthenticated,]
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        user = request.user
+
+        upvote, created = Upvote.objects.get_or_create(user=user, comment=instance)
+
+        if not created:
+            return Response({'detail': 'You have already upvoted this comment.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
