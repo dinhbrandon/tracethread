3cd3bbad2b8bfa38604cd2f8f966caad29c6b862
karbassioon-d <karbasion.dara@gmail.com>
2023-11-07 14:42:51 -0800

    optimized mobile nav menu

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -77,6 +77,8 @@ const Nav = () => {
 
   // const [isMenuOpen, toggleMenu] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
+  const [isMenuOpen, toggleMenu] = useState(false);
+
   const pageUrl = window.location.href;
 
   const isActive = (path: any) => location.pathname === path;
@@ -87,69 +89,70 @@ const Nav = () => {
   };
 
   return (
-      <header className="border-b flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full py-3 sm:py-0">
+    <header className="border-b flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full py-3 sm:py-0">
       <nav className="relative w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8" aria-label="Global">
-
-      <div className="flex items-center">
-    <div>
-        <a className="flex-none text-xl font-semibold whitespace-nowrap text-gray-700" href={baseUrl} aria-label="Tracethread">Trace Thread BETA</a>
-    </div>
-
-    <div className='ml-10 flex items-center'>
-    <button
-    onClick={() => setModalOpen(true)}
-    className="m-1 py-2 px-3 rounded-md border font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 transition-all text-sm flex items-center min-w-[170px]"
-    >
-        <img className='h-8 w-8 rounded-full ring-2 ring-blue mr-2'
-            src="https://img.icons8.com/?size=96&id=20544&format=png" 
-            alt="Feedback" 
-        />
-        <span className='whitespace-nowrap'>Fix/Suggestions</span>
-    </button>
-    <SubmitFeedback isOpen={modalOpen} onClose={() => setModalOpen(false)} pageUrl={pageUrl} />
-</div>
-
-
-
-
-
-
-</div>
-
+        <div className='flex items-center justify-between'>
+          <a className="flex-none text-xl font-semibold whitespace-nowrap text-gray-700" href={baseUrl} aria-label="Tracethread">Trace Thread BETA</a>
+          <div className='lg:ml-10'>
+              <button
+                onClick={() => setModalOpen(true)}
+                className="m-1 py-2 px-3 rounded-md border font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 transition-all text-sm flex items-center min-w-[170px]"
+              >
+                <img className='h-8 w-8 rounded-full ring-2 ring-blue mr-2'
+                  src="https://img.icons8.com/?size=96&id=20544&format=png" 
+                  alt="Feedback" 
+                />
+                <span className='whitespace-nowrap'>Fix/Suggestions</span>
+              </button>
+              <SubmitFeedback isOpen={modalOpen} onClose={() => setModalOpen(false)} pageUrl={pageUrl} />
+          </div>
+          <div className="flex justify-between items-center">
+            <div className='flex justify-between'>
+              <div className="sm:hidden">
+                <button onClick={() => toggleMenu(!isMenuOpen)} type="button" className="p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium shadow-sm align-middle focus:outline-none focus:ring-2 focus:ring-offset-2 transition-all text-sm" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
+                    <svg className="w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                    <svg className="hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                    </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
         
-
-        
-        <div id="navbar-collapse-with-animation" className={`overflow-hidden transition-all duration-300 basis-full grow sm:block`}>
+        <div id="navbar-collapse-with-animation" className={`${isMenuOpen ? '' : 'hidden'} overflow-hidden transition-all duration-300 basis-full grow sm:block`}>
           <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
-            { loggedIn ? (
+            {loggedIn ? (
               <>
                 <a href={`${baseUrl}/feedback`} className={`font-medium ${isActive('/feedback') ? 'border-b-2 border-blue-500' : ''}`}>Feedback Thread</a>
                 <a href={`${baseUrl}/dashboard`} className={`font-medium ${isActive('/dashboard') ? 'border-b-2 border-blue-500' : ''}`}>Info</a>
                 <a href={`${baseUrl}/search`} className={`font-medium ${isActive('/search') ? 'border-b-2 border-blue-500' : ''}`}>Search</a>
                 <a href={`${baseUrl}/jobnotebook`} className={`font-medium ${isActive('/jobnotebook') ? 'border-b-2 border-blue-500' : ''}`}>Job Notebook</a>
               </>
-            ) : <></>}
-            
-              {loggedIn ? (
+            ) : null}
+            {loggedIn ? (
               <button onClick={handleLogout} className="flex items-center gap-x-2 font-medium sm:border-l sm:my-6 sm:pl-6">
                 <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                 </svg>
                 Logout
               </button>
-              ) : (
+            ) : (
               <a className="flex items-center gap-x-2 font-medium sm:border-l sm:my-6 sm:pl-6" href={`${baseUrl}/login`}>
                 <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                 </svg>
                 Login
               </a>
-              )}
+            )}
           </div>
         </div>
       </nav>
     </header>
   );
+  
   
 };
 
